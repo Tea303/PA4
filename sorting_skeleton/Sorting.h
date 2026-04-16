34ffbf6b17cebdefbca0f0f7eccfe3e6f748a455
Tea303 <yutaace.raqueno@wsu.edu>
2026-04-15 23:06:03 -0700

    sorting

---
+++ sorting_skeleton/Sorting.h
@@ -0,0 +1,79 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+#include <vector>
+
+//------------------------------------------------------------------
+// insertion sort
+//-------------------------------------------------------------------
+
+template <typename T, typename Comparator>
+void insertionSort(std::vector<T>& data, Comparator comp) {
+    for (size_t i = 1; i < data.size(); ++i) {
+        T key = data[i];
+        int j = i - 1;
+
+        // move elements that's greater than key to one position ahead
+        // until the correct position for key is found
+        while (j >= 0 && comp(key, data[j])) {
+            data[j + 1] = data[j];
+            --j;
+        }
+        data[j + 1] = key;
+    }
+}
+
+//------------------------------------------------------------------
+// merge sort
+//-------------------------------------------------------------------
+
+template <typename T, typename Comparator>
+void merge(std::vector<T>& data, int left, int mid, int right, Comparator comp) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    // temp vectors
+    std::vector<T> lData(n1);
+    std::vector<T> rData(n2);
+
+    // copy data to temp vectors
+    for (int i = 0; i < n1; ++i) {
+        lData[i] = data[left + i];
+    }
+    for (int j = 0; j < n2; ++j) {
+        rData[j] = data[mid + 1 + j];
+    }
+
+    // merge temp vectors back into data
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (comp(lData[i], rData[j])) {
+            data[k] = lData[i];
+            ++i;
+        } else {
+            data[k] = rData[j];
+            ++j;
+        }
+        ++k;
+    }
+
+    // copy remaining elements of lData
+    while (i < n1) {
+        data[k] = lData[i];
+        ++i;
+        ++k;
+    }  
+
+    // copy remaining elements of rData
+    while (j < n2) {
+        data[k] = rData[j];
+        ++j;
+        ++k;
+    }
+}
+
+
+
+
+
+#endif
